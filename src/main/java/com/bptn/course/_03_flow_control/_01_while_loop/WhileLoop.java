c53f443f95892a5b10badb4d7cc3013d351123d4
Odogwu2 <udamiananumudu@gmail.com>
2024-09-25 22:01:23 -0400

    adding while loop sample to while loop package

--- src/main/java/com/bptn/course/_03_flow_control/_01_while_loop/WhileLoop.java
+++ src/main/java/com/bptn/course/_03_flow_control/_01_while_loop/WhileLoop.java
@@ -4,5 +4,15 @@ public class WhileLoop {
 
 	public static void main(String[] args) {
 
+		int counter = 1;
+
+		// start the while loop
+		while (counter < 10) {
+			System.out.println("Counter: " + counter);
+
+			// increment the counter
+			counter++;
+		}
+
 	}
 }
