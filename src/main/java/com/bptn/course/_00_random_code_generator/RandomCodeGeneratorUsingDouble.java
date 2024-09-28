c88c133f0e9167af9a3fca02fe4bad57414f3b2b
Odogwu2 <udamiananumudu@gmail.com>
2024-09-28 13:39:49 -0400

    updating randome code generator

---
+++ src/main/java/com/bptn/course/_00_random_code_generator/RandomCodeGeneratorUsingDouble.java
@@ -0,0 +1,23 @@
+package com.bptn.course._00_random_code_generator;
+
+import java.util.Random;
+
+public class RandomCodeGeneratorUsingDouble {
+
+	public static void main(String[] args) {
+		Random rand = new Random();
+
+		double randomNumber = (double) Math.random();
+
+		// int intValue = (int) randomNumber; (Cast the double to an int)
+		// using double to allow for decimal values
+		// return type of the method which determines what data type the variable will
+		// be
+		// Math.random generates a random number
+		// datatype variableName = objectName.method();
+
+		System.out.println("After execution the Random number is: " + randomNumber);
+
+	}
+
+}
