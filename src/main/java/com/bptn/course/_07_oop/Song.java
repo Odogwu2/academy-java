3fa396fc8045125645ca8e4621f4fd54ecc6aa94
Odogwu2 <udamiananumudu@gmail.com>
2024-09-30 13:08:18 -0400

    Adds OOP package and class examples

---
+++ src/main/java/com/bptn/course/_07_oop/Song.java
@@ -0,0 +1,24 @@
+package com.bptn.course._07_oop;
+
+public class Song {
+
+	// The chorus method
+	public static void chorus() {
+		System.out.println("I'm looking over a four-leaf clover");
+		System.out.println("That I overlooked before");
+	}
+
+	public static void main(String[] args) {
+
+		chorus();
+		System.out.println("One leaf is sunshine, the second is rain");
+		System.out.println("Third is the roses that grow in the lane");
+		System.out.println();
+		System.out.println("No need explaining, the one remaining");
+		System.out.println("Is somebody I adore");
+		// Look at the previous version above and finish the song with a method call
+		// here:
+
+	}
+
+}
