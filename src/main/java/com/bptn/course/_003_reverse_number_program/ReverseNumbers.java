36cd2940a39704cdb856b836eac6432ad4facb57
Odogwu2 <udamiananumudu@gmail.com>
2024-09-29 21:06:12 -0400

    updating code

--- src/main/java/com/bptn/course/_003_reverse_number_program/ReverseNumbers.java
+++ src/main/java/com/bptn/course/_003_reverse_number_program/ReverseNumbers.java
@@ -5,19 +5,25 @@ import java.util.Scanner;
 public class ReverseNumbers {
 
 	public static void main(String[] args) {
-		Scanner scanner = new Scanner(System.in);
+		Scanner scanner = new Scanner(System.in); // initializing the scanner object. System.in is responsible for user input. 
 
-        System.out.print("Enter a number: ");
-        int number = scanner.nextInt();
+        System.out.print("Enter a number: "); // call to action for the user to enter a number. 
+        int number = scanner.nextInt(); //reads the integer entry and stores the input in "number" which is of the integer data type. 
 
-        int reversedNumber = 0;
-        while (number != 0) {
-            int lastDigit = number % 10;
-            reversedNumber = reversedNumber * 10 + lastDigit;
-            number /= 10; //or number = number / 10; 
+        int reversedNumber = 0; // declare a variable and initialize with a 0 value. the initial value will be used later to calculate a number.  
+        while (number != 0) // as long as the number is not zero, the loop below is going to run and only stop when all the digits in number have been processed.
+        //Checks the condition before executing the code. 
+        {
+            int lastDigit = number % 10; // to get the last digit of any number, we will be getting the remainder when the number is divided by 10. 
+            reversedNumber = reversedNumber * 10 + lastDigit;  //Add that digit to reversedNumber, shifting its existing digits to the left by multiplying by 10.
+            number /= 10; //or number = number / 10; This ensures that the next iteration of the loop works on the remaining digits
         }
 
+//        	for (number !=0; reversedNumber = reversedNumber * 10 + lastDigit; number/=10) {
+//        		intlastDigit = number % 10;
+//        	}
         System.out.println("Reversed number: " + reversedNumber); 
+        scanner.close();
 
 	}
 	
--- src/main/java/com/bptn/course/_06_big_coding_challenges/StringPluralizer.java
+++ src/main/java/com/bptn/course/_06_big_coding_challenges/StringPluralizer.java
@@ -11,86 +11,95 @@ public class StringPluralizer {
 		//number must be a +ve value 
 		//Ask the user to enter a word 
 		
-		Scanner sc = new Scanner(System.in);
+		Scanner scanner = new Scanner(System.in); // Initializing scanner object to read input from the user. 
 		
-		int amt = 0; 
-		String word; 
-		String endsWithY = "ies";
-		String endsWithFe = "ves"; 
-		String endsWithUs = "i";
+		int num = 0; // declaring variable for number value to be entered by the user. 
+		String word; // declare variable for the word object to be entered by the user. 
+		String endsWithY = "ies"; //new characters to be added when words end with "y" (after y is removed from the original string) 
+		String endsWithFe = "ves"; //new characters to be added when words end with "fe" (after fe is removed from the original string)
+		String endsWithUs = "i"; //new characters to be added when words end with "us" (after us is removed from the original string)
 		
 		
-		System.out.println ("Enter a number: "); 
-		amt = sc.nextInt();
+		System.out.println ("Enter a number: "); //first message on the console 
+		num = scanner.nextInt(); //scanner will read the integer input from the user and store it in amount. 
 		
-		if (amt < 0) {
-			System.out.println ("Enter zero or a positive number");
-			return;
+		if (num < 0) {
+			System.out.println ("Enter zero or a positive number"); //return message if the user enters a value less than 0
+			return; // break point to end the program since number is not zero or a positive value. 
 		}
 		
-		//do {
-		sc.nextLine();
-		System.out.println ("Enter a word: ");
-		word = sc.nextLine(); 
 		
-		if (amt == 1) {
-			System.out.println(amt + " " + word);
+		scanner.nextLine(); //ensuring that the subsequent nextLine() reads the actual word input.
+		System.out.println ("Enter a word: "); 
+		word = scanner.nextLine(); //reads the string and stores the input in "word"
+		
+		//here is where we begin the pluralization logic. 
+		if (num == 1) //checks if the number is 1. 
+		{ 
+			System.out.println(num + " " + word); 
+			//since the input is 1, there's no need to pluralize the word. 
+			//Return the number entered and the word. The empty quote is a space. 
 			
-		/*} 
-		//else if (amt < 0){
-			//System.out.println ("Enter zero or a positive number"); 
+		} else if (word.endsWith("ay") || word.endsWith("oy") || word.endsWith("ey") || word.endsWith("uy")) //creating our condition for special cases 
+			//using an OR operation. Only one condition has to be true for this action to happen 
+			//and a endsWith(String suffix) method of the string class. This checks the letters specified in the (String suffix)
+		{
+			System.out.println(num + " " + word + "s"); //concatenating the number inputed with the word and adding an 's' . 
+			//Here we have not removed any character from the word entered. 
+			
+		/*else if (amt >= 0 && word.endsWith("y")) {
+			//String modifiedForY = word.substring(0, word.length() -1) + endsWithY; 
+			//System.out.println(amt + " " +modifiedForY);
+			
+			//else (amt >= 0 && word.endsWith("ay") || word.endsWith("oy") || word.endsWith("ey") || word.endsWith("uy")) {
+			//System.out.println(amt + " " + "s");	
 			 */
 			
 		} else if (word.endsWith("y")) {
-			String modifiedForY = word.substring(0, word.length() -1) + endsWithY; 
-			System.out.println(amt + " " +modifiedForY);
+			String modifiedForY = word.substring(0, word.length() -1) + endsWithY; //using the substring method of the string class to index characters in a string 
+			//This part creates a new substring from word, starting from index 0 (the first character) to the index word.length() - 1 (which is the index of the last character).
+			//This is a method used to remove the last character of the string word and then we append the string variable "endsWithY. 
+			System.out.println(num + " " + modifiedForY);
 			
-		} else if (amt >= 0 && word.endsWith("y")) {
-			String modifiedForY = word.substring(0, word.length() -1) + endsWithY; 
-			System.out.println(amt + " " +modifiedForY);
-			
-		} else if (amt >= 0 && word.endsWith("fe")) {
+		} else if (word.endsWith("fe")) {
 			String modifiedForFe = word.substring(0, word.length() -2) + endsWithFe; 
-			System.out.println(amt + " " +modifiedForFe);
+			//By subtracting 2 here, we are creating a new substring that removes the last two characters from the string "word".
+			//We then assign this new word to modifiedForFe variable. 
+			System.out.println(num + " " + modifiedForFe);
 			
-		} //else if (amt == 1) {
-			//System.out.println(amt + " " + word); 
-			
-		//} 
-			else if (amt >= 0 && word.endsWith("us")) {
+		} else if (word.endsWith("us")) {
 			String modifiedForUs = word.substring(0, word.length() -2) + endsWithUs; 
-			System.out.println(amt + " " +modifiedForUs);
+			//By subtracting 2 here, we are creating a new substring that removes the last two characters from the string "word".
+			//We then assign this new word to modifiedForUs variable. 
+			System.out.println(num + " " + modifiedForUs);
 			
-		} else if (amt >= 0 && word.endsWith("sh")) {
-			String modifiedForSh = word + "es"; 
-			System.out.println(amt + " " + modifiedForSh);
+		} else if (word.endsWith("sh")) //as with previous steps, using a method in the String class, we are checking if the current string (in this case, word) ends with the specified suffix 
+			//which is "sh" in this context. 
+		
+		{
+			String modifiedForSh = word + "es"; //and we add es to the word. 
+			System.out.println(num + " " + modifiedForSh);
 			
-		} else if (amt >= 0 && word.endsWith("ch")) {
+		} else if (word.endsWith("ch")) {
 			String modifiedForCh = word + "es"; 
-			System.out.println(amt + " " + modifiedForCh);
+			System.out.println(num + " " + modifiedForCh);
 			
-		} else if (amt < 0){
-			System.out.println ("Enter zero or a positive number"); 
-		} else if (amt >= 0) {
-			System.out.println (amt + " " + word + "s"); 
 		} else {
-			System.out.println (amt + " " + word + "s");
+			System.out.println (num + " " + word + "s"); //our default condition is set here. 
 		}
-		sc.close();
+		scanner.close();
 		
-		//else (amt >= 0 && word.endsWith("ay") || word.endsWith("oy") || word.endsWith("ey") || word.endsWith("uy")) {
-			//System.out.println(amt + " " + "s"); 
-			
-		//}
-		
-		//else if (amt <=0) 
-		
-		//} while (amt == 0);
 
 	}
 
 }
 
+
+
+//} else if (amt < 0){
+//System.out.println ("Enter zero or a positive number"); 
+//} else if (amt >= 0) {
+//System.out.println (amt + " " + word + "s"); 
 
 //if (amt < 0) {
 	//System.out.println("Please enter a zero or positive number.");
